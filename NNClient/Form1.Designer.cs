66112986c5f6adb901fe18c69283b884fb1e2ad8
N1x9d <nsh19021999@yandex.ru>
2021-11-23 04:36:23 +0500

    фикс

--- NNClient/Form1.Designer.cs
+++ NNClient/Form1.Designer.cs
@@ -43,9 +43,9 @@ namespace NNClient
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(706, 119);
+            this.button1.Location = new System.Drawing.Point(694, 119);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 31);
+            this.button1.Size = new System.Drawing.Size(94, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "Распознать";
             this.button1.UseVisualStyleBackColor = true;
