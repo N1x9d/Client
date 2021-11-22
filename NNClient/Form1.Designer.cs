a9ad33ec8858b9aa37c1a5c0a75a98ccaa92074d
N1x9d <nsh19021999@yandex.ru>
2021-11-23 04:32:12 +0500

    русификация

--- NNClient/Form1.Designer.cs
+++ NNClient/Form1.Designer.cs
@@ -47,7 +47,7 @@ namespace NNClient
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(82, 31);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Analize";
+            this.button1.Text = "Распознать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -57,9 +57,9 @@ namespace NNClient
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(539, 91);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.Size = new System.Drawing.Size(155, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Picture type";
+            this.label1.Text = "Тип изображения";
             // 
             // pictureBox1
             // 
@@ -73,11 +73,11 @@ namespace NNClient
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(192, 181);
+            this.label2.Location = new System.Drawing.Point(127, 169);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 25);
+            this.label2.Size = new System.Drawing.Size(239, 25);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Input image";
+            this.label2.Text = "Загрузите изображение";
             // 
             // textBox1
             // 
@@ -94,7 +94,7 @@ namespace NNClient
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(84, 34);
             this.button2.TabIndex = 5;
-            this.button2.Text = "Search";
+            this.button2.Text = "Обзор";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -105,9 +105,9 @@ namespace NNClient
             this.label3.ForeColor = System.Drawing.Color.Red;
             this.label3.Location = new System.Drawing.Point(539, 130);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.Size = new System.Drawing.Size(105, 20);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Unknown";
+            this.label3.Text = "Не извесен";
             // 
             // textBox2
             // 
@@ -122,9 +122,9 @@ namespace NNClient
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(543, 39);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.Size = new System.Drawing.Size(78, 17);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Server IP";
+            this.label4.Text = "IP сервера";
             // 
             // Form1
             // 
